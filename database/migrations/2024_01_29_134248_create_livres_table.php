83b6eb5ad14cd8f2e4f24317ce6a57ec845627ea
souf139 <soufianboushaba12@gmail.com>
2024-01-29 15:13:45 +0100

    this commit is for creating the code for database and  make model and  migration

--- database/migrations/2024_01_29_134248_create_livres_table.php
+++ database/migrations/2024_01_29_134248_create_livres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLivresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('livres', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('titre');
+            $table->string('auteur');
+            $table->integer('annee_publication');
+            $table->string('statut');
             $table->timestamps();
             $table->engine = "InnoDB";
         });
     }
+    
 
     /**
      * Reverse the migrations.
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('livres');
     }
 }
--- database/migrations/2024_01_29_134431_create_reservations_table.php
+++ database/migrations/2024_01_29_134431_create_reservations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReservationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('id_livre')->constrained('livres');
+            $table->foreignId('id_user')->constrained('users');
+            $table->date('date_start');
+            $table->date('date_fin');
+            $table->string('statut');
             $table->timestamps();
             $table->engine = "InnoDB";
         });
     }
+    
 
     /**
      * Reverse the migrations.
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 }
