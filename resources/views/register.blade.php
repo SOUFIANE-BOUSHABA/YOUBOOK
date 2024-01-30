9768e53facdc46c049c14ef40135d3b8e29fcfb8
souf139 <soufianboushaba12@gmail.com>
2024-01-30 17:25:24 +0100

    this commit is for remove required in html for creat it  in validate in store

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -22,22 +22,22 @@
 
                         <div class="mb-3">
                             <label for="name" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required autofocus>
+                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}"  autofocus>
                         </div>
 
                         <div class="mb-3">
                             <label for="email" class="form-label">E-Mail Address</label>
-                            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
+                            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" >
                         </div>
 
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
+                            <input type="password" class="form-control" id="password" name="password" >
                         </div>
 
                         <div class="mb-3">
                             <label for="password_confirmation" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" >
                         </div>
 
                         <button type="submit" class="btn btn-primary">Register</button>
